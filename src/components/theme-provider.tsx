fa5fdad44b767aaa83b50514345b9587750a25e1
Carlos Ramos <carlosramos@Carloss-MacBook-Air.local>
2024-01-08 22:56:33 -0500

    removed console logs

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -5,13 +5,11 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-	console.log({ props });
 	useEffect(() => {
-		console.log("useEffect");
 		const body = document.querySelector("body");
 		if (body) {
 			body.classList.remove("dark");
 		}
-	}, []);
+	}, [props]);
 	return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
