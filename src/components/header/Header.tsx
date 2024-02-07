3a60163b48924da8110388c92ba7f5f4357689b8
Carlos Ramos <carlosramos@Carloss-MacBook-Air.local>
2024-02-07 13:42:30 -0500

    partly completed theming issue

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,14 @@
 import MobileMenu from "@/components/mobilemenu/MobileMenu";
+import Link from "next/link";
 
 const Header: React.FC = () => {
 	return (
 		<header className="flex justify-between items-center px-3 h-[6rem]">
-			<h1>Vine Church</h1>
+			<Link href="/">
+				<h1 className="uppercase text-lg">
+					Vine <span className="font-bold">Church</span>
+				</h1>
+			</Link>
 			<MobileMenu />
 		</header>
 	);
--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,15 +1,23 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
+import { usePathname } from "next/navigation";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-	useEffect(() => {
-		const body = document.querySelector("body");
-		if (body) {
-			body.classList.remove("dark");
-		}
-	}, [props]);
+	const pathname = usePathname();
+
+	let theme = "light";
+	if (typeof window !== "undefined") {
+		theme = localStorage.getItem("theme") || "light";
+	}
+
+	const html = typeof window !== "undefined" && document.documentElement;
+	if (html && theme === "dark" && !pathname.startsWith("/sermons/")) {
+		localStorage.setItem("theme", "light");
+		html.setAttribute("class", "light");
+	}
+
 	return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
