46e6d433a6b3982c0c894a13a38aeb3cef785ac6
Carlos Ramos <carlosramos@Carloss-MacBook-Air.local>
2024-03-20 23:57:29 -0400

    give page is desktop supported

--- src/app/(user)/give/page.tsx
+++ src/app/(user)/give/page.tsx
@@ -50,19 +50,23 @@ const textGivingData: TextGivingDataType[] = [
 export default function GivingPage() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between pb-14 max-w-sm px-2 lg:max-w-7xl mx-auto">
-			<div className="bg-white flex justify-center items-center h-[400px] rounded-lg shadow-[rgba(67,_186,_182,_0.1)_0px_9px_30px] mb-10">
-				<h1 className="text-center font-semibold text-xl px-4 text-vinegreen">
+			<div className="relative flex justify-center items-center h-[400px] mb-10">
+				<div className="absolute -z-10 h-full w-full bg-[radial-gradient(30em_circle_at_50%_200px,#43bab6,transparent_40%)] opacity-30 mx-auto top-0 inset-x-0;" />
+				<h1 className="text-left font-bold text-4xl px-4 text-shadow text-black lg:w-2/4 lg:text-center lg:text-6xl">
 					See what God can do through your generosity.
 				</h1>
 			</div>
 
-			<div className="my-10 text-center bg-white text-darkbg p-4 rounded-lg shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-				<h3 className="font-semibold text-3xl mb-4">Online Giving</h3>
-				<p className="mb-4">
-					Simple and secure. Giving Online is the easiest and best way to give.
-				</p>
+			<div className="my-10 text-center bg-white text-darkbg p-4 rounded-lg shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] lg:w-full lg:max-w-7xl lg:flex lg:items-center lg:justify-center lg:p-10">
+				<div>
+					<h3 className="font-semibold text-3xl mb-4 lg:mb-0">Online Giving</h3>
+					<p className="mb-4 lg:text-xl lg:w-1/2 lg:mx-auto">
+						Simple and secure. Giving Online is the easiest and best way to
+						give.
+					</p>
+				</div>
 
-				<div className="flex flex-col justify-center items-center">
+				<div className="flex flex-col justify-center items-center lg:w-1/2 lg:mx-auto">
 					{waysToGiveData.map(({ title, link }: WaysToGiveType) => (
 						<Link
 							key={title}
@@ -75,30 +79,38 @@ export default function GivingPage() {
 					))}
 				</div>
 			</div>
-			<div className="my-10 text-center bg-white text-darkbg p-4 rounded-lg shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-				<h3 className="font-semibold text-3xl mb-4">Text Giving</h3>
-				<p className="mb-4">
-					Text giving uses industry-leading security to protect your personal
-					information, and is never charged to your phone bill.
-				</p>
-				<Image className="rounded-lg" src={giveTextGif} alt="give text gif" />
-				<div>
-					{textGivingData.map(
-						({ id, number, description, example }: TextGivingDataType) => (
-							<div
-								key={id + description}
-								className="flex justify-between items-center gap-5"
-							>
-								<div className="bg-black/5 w-3/12 h-[4.5rem] font-bold text-[2rem] text-darkbg rounded-md flex justify-center items-center m-[1rem]">
-									{number}
+			<div className="my-10 text-center bg-white text-darkbg p-4 rounded-lg shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] lg:w-full lg:max-w-7xl lg:p-10">
+				<div className="lg:mb-10">
+					<h3 className="font-semibold text-3xl mb-4">Text Giving</h3>
+					<p className="mb-4 lg:text-xl lg:w-1/2 lg:mx-auto">
+						Text giving uses industry-leading security to protect your personal
+						information, and is never charged to your phone bill.
+					</p>
+				</div>
+				<div className="lg:flex lg:items-center lg:justify-center lg:gap-10">
+					<Image
+						className="rounded-lg lg:w-[400px]"
+						src={giveTextGif}
+						alt="give text gif"
+					/>
+					<div>
+						{textGivingData.map(
+							({ id, number, description, example }: TextGivingDataType) => (
+								<div
+									key={id + description}
+									className="flex justify-between items-center gap-5"
+								>
+									<div className="bg-black/5 w-3/12 h-[4.5rem] font-bold text-[2rem] text-darkbg rounded-md flex justify-center items-center m-[1rem]">
+										{number}
+									</div>
+									<div className="text-left w-3/4">
+										<h4 className="text-sm font-bold">{description}</h4>
+										<p className="text-lg">{example}</p>
+									</div>
 								</div>
-								<div className="text-left w-3/4">
-									<h4 className="text-sm font-bold">{description}</h4>
-									<p className="text-lg">{example}</p>
-								</div>
-							</div>
-						)
-					)}
+							)
+						)}
+					</div>
 				</div>
 			</div>
 		</main>
