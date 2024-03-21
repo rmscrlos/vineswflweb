b83126b52ee6de15800f55e6b10a17ab8c1b072b
Carlos Ramos <carlosramos@Carloss-MacBook-Air.local>
2024-03-21 00:04:12 -0400

    grow page coming soon

--- src/app/(user)/grow/page.tsx
+++ src/app/(user)/grow/page.tsx
@@ -1,8 +1,10 @@
 export default function GrowPage() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between pb-14">
-			<h1>Grow Page</h1>
-			{/* Add your content here */}
+		<main className="flex min-h-screen flex-col items-center justify-center pb-14">
+			<h1 className="text-left font-bold text-4xl px-4 text-shadow text-black lg:w-2/4 lg:text-center lg:text-6xl">
+				Grow Page
+			</h1>
+			<p>coming soon.</p>
 		</main>
 	);
 }
